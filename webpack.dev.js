d1e3868c454a65e248188ce8c44a29017a4583ba
AlburIvan <albur.ivan@outlook.com>
2018-11-15 03:00:47 -0400

    [WIP] configuring hot reload for scss, js & html

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,20 @@
 
+/**
+ * to access built-in plugin.
+ * https://webpack.js.org/concepts/#plugins
+ */
+const webpack = require('webpack');
+
 /** 
- * Simplifies creation of HTML files to serve your webpack bundles 
+ * Simplifies creation of HTML files to serve your webpack bundles.
  * https://github.com/jantimon/html-webpack-plugin
  */
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
+
+  mode: 'development',
 
   // entry point for webpack to begin compiling
   entry: {
@@ -16,13 +24,33 @@ module.exports = {
 
   // dev Server setup
   devServer: {
-    https: true,
-    port: 5555,
+    port: 9000,
     compress: true,
     hot: true,
     historyApiFallback: {
-      rewrites: [{ from: /./, to: 'views/errors/404.html' }]
+      rewrites: [
+        { from: /./, to: 'views/errors/404.html' }
+      ]
     }
+  },
+
+  // Here be modules
+  module: {
+    rules: [
+      { 
+        test: /\.css|scss$/, 
+        use: [ 
+          'style-loader', // creates style nodes from JS strings
+          'css-loader', // translate CSS into CommonJS
+          'sass-loader' // compiles Sass to CSS, using Node Sass by default
+        ]
+      },
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/,
+        use: ['babel-loader'] // transpile ECMA6 code to vanilla JS with babel
+      }
+    ]
   },
 
   // Here be plugins
@@ -31,6 +59,7 @@ module.exports = {
       template: './views/index.html',
       inject: true,
       chunks: ['index'],
+      title: 'My App',
       filename: 'index.html'
     }),
     new HtmlWebpackPlugin({
@@ -38,6 +67,11 @@ module.exports = {
       inject: true,
       chunks: ['about'],
       filename: 'about.html'
+    }),
+    new webpack.DefinePlugin({
+      NODE_ENV: JSON.stringify("development"),
+      PRODUCTION: JSON.stringify(false),
+      VERSION: JSON.stringify('5fa3b9'),
     })
   ]
 }
